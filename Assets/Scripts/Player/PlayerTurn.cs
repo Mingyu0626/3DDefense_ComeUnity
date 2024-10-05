52f39112330303d2e3cb5da62f2f83c65e0b4ef9
Mingyu Choi <mg010626@naver.com>
2024-10-05 17:56:45 +0900

    Feat | 카메라 X축 회전 클램핑 적용

--- Assets/Scripts/Player/PlayerTurn.cs
+++ Assets/Scripts/Player/PlayerTurn.cs
@@ -8,14 +8,18 @@ public class PlayerTurn : MonoBehaviour
 {
     PlayerInputAction action;
     InputAction turnAction;
-    PlayerAnimation playerAnimation;
-    private float turnSpeed = 0.1f;
+    Camera mainCamera;
+    private float turnSpeed = 0.06f;
+    private float minXRotation = -15f;
+    private float maxXRotation = 10f;
+    private float curXRotation = 0f;
 
     private void Awake()
     {
         action = new PlayerInputAction();
         turnAction = action.Player.Turn;
         turnAction.Enable();
+        mainCamera = transform.Find("Main Camera").GetComponent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -29,6 +33,19 @@ public class PlayerTurn : MonoBehaviour
     void Update()
     {
         Vector2 mouseDelta = turnAction.ReadValue<Vector2>();
-        transform.Rotate(0, mouseDelta.x * turnSpeed, 0);
+        TurnYAxis(mouseDelta);
+        TurnXAxis(mouseDelta);
+    }
+
+    void TurnYAxis(Vector2 delta)
+    {
+        transform.Rotate(0, delta.x * turnSpeed, 0);
+    }
+
+    void TurnXAxis(Vector2 delta)
+    {
+        curXRotation -= delta.y * turnSpeed;
+        curXRotation = Mathf.Clamp(curXRotation, minXRotation, maxXRotation);
+        mainCamera.transform.localEulerAngles = new Vector3(curXRotation, 0, 0);
     }
 }
