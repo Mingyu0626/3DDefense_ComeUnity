254bf4b4fb775067a97c3396ace34aa206bbd8ac
Mingyu0626 <mg010626@naver.com>
2024-11-03 15:58:14 +0900

    Style | PlayerCamera 스크립트 CameraAction으로 이름 변경

--- Assets/Scripts/Camera/CameraAction.cs
+++ Assets/Scripts/Camera/CameraAction.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerCamera : MonoBehaviour
+public class CameraAction : MonoBehaviour
 {
     PlayerInputAction action;
     InputAction turnAction;
     Camera mainCamera;
 
-    public Transform playerTransform;
+    [SerializeField]
+    private Transform playerTransform;
 
     private float turnSpeed = 0.2f;
     private float minXRotation = -10f;
     private float maxXRotation = 30f;
-    private float XRotation = 0f;
-    private float YRotation = 0f;
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
     private float rotationTime = 0.1f;
     private float distanceFromPlayer = 15f;
+    private float yPositionCorrection = 8f;
 
     private Vector3 targetRotation;
     private Vector3 currentVelocity;
@@ -42,24 +45,24 @@ public class PlayerCamera : MonoBehaviour
         TurnYAxis(mouseDelta);
         TurnXAxis(mouseDelta);
 
-        targetRotation = Vector3.SmoothDamp(targetRotation, new Vector3(XRotation, YRotation),
+        targetRotation = Vector3.SmoothDamp(targetRotation, new Vector3(xRotation, yRotation),
             ref currentVelocity, rotationTime);
         transform.eulerAngles = targetRotation;
         transform.position = playerTransform.position - transform.forward * distanceFromPlayer;
-        transform.position += Vector3.up * 8f;
+        transform.position += Vector3.up * yPositionCorrection;
 
         UpdatePlayerRotation();
     }
 
     void TurnYAxis(Vector2 delta)
     {
-        YRotation += delta.x * turnSpeed;
+        yRotation += delta.x * turnSpeed;
     }
 
     void TurnXAxis(Vector2 delta)
     {
-        XRotation -= delta.y * turnSpeed;
-        XRotation = Mathf.Clamp(XRotation, minXRotation, maxXRotation);
+        xRotation -= delta.y * turnSpeed;
+        xRotation = Mathf.Clamp(xRotation, minXRotation, maxXRotation);
     }
 
     void UpdatePlayerRotation()
