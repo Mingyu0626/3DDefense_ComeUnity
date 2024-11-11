06fda53b323a774c29df20eb10422a632fb546c0
Mingyu Choi <mg010626@naver.com>
2024-11-11 17:55:10 +0900

    Feat | HPComponent의 OnTriggerEnter 및 ApplyDamage 가상화 및 자식 클래스에서 부모 버전 호출하도록 구현

--- Assets/Scripts/Component/HPComponent.cs
+++ Assets/Scripts/Component/HPComponent.cs
@@ -8,17 +8,16 @@ public class HPComponent : MonoBehaviour
 
     [SerializeField]
     public int MaxHP { get; private set; } = 100;
-    private int curHP;
+    protected int curHP;
 
     protected virtual void Awake()
     {
         curHP = MaxHP;
     }
-    public void ApplyDamage(int damage)
+    protected virtual void ApplyDamage(int damage)
     {
         curHP -= damage;
         curHP = Mathf.Clamp(curHP, 0, MaxHP);
-        UIManager.Instance.SetPlayerHPSlider(curHP);
     }
 
     public bool IsHPZero()
@@ -26,12 +25,15 @@ public class HPComponent : MonoBehaviour
         return curHP == 0;
     }
 
-    private void OnTriggerEnter(Collider other)
+    protected virtual void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("EnemyAttack"))
         {
             Bullet EnemyAttack = other.GetComponent<Bullet>();
-            ApplyDamage(EnemyAttack.GetDamage());
+            if (EnemyAttack != null)
+            {
+                ApplyDamage(EnemyAttack.GetDamage());
+            }
             if (IsHPZero())
             {
                 // 플레이어 사망 or 기지 파괴에 의한 게임 종료
