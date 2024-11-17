1471886526e4922110932d8e72352718ebd5f34c
Mingyu0626 <mg010626@naver.com>
2024-11-17 14:50:36 +0900

    Style | 불필요 주석 제거

--- Assets/Scripts/Session Exercise Scripts/StageManagerExercise.cs
+++ Assets/Scripts/Session Exercise Scripts/StageManagerExercise.cs
@@ -48,10 +48,8 @@ public class StageManagerExercise : MonoBehaviour
         // 현재 스테이지가 마지막 스테이지라면,
         // GameManager의 isWin 변수를 true로 변경
         // GameManager의 EndGame을 호출
-        Debug.Log("Clearstage() 호출");
         if (currentStage == numOfStages)
         {
-            Debug.Log("게임 클리어");
             GameManager.Instance.EndGame(true);
         }
         currentStage++;
