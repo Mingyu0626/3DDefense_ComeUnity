c44f510bbce5d7a8cc1aabbd1aa6785831a9f6ca
Mingyu Choi <mg010626@naver.com>
2024-12-13 20:29:50 +0900

    Feat | 환경설정 패널 제어 스크립트 SettingsPanel 구현

---
+++ Assets/Scripts/Settings/SettingsPanel.cs
@@ -0,0 +1,89 @@
+using Settings;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class SettingsPanel : MonoBehaviour
+{
+    [SerializeField] private Button displaySettingsButton;
+    [SerializeField] private Button graphicSettingsButton;
+    [SerializeField] private Button gameplaySettingButton;
+    [SerializeField] private Button soundSettingsButton;
+
+    [SerializeField] private GameObject displaySettingsGO;
+    [SerializeField] private GameObject graphicSettingsGO;
+    [SerializeField] private GameObject gameplaySettingGO;
+    [SerializeField] private GameObject soundSettingsGO;
+
+    [SerializeField] private Button applyButton;
+    [SerializeField] private Button closeButton;
+
+    private void Awake()
+    {
+        displaySettingsButton.onClick.AddListener(OnClickDisplaySettings);
+        graphicSettingsButton.onClick.AddListener(OnClickGraphicSettings);
+        gameplaySettingButton.onClick.AddListener(OnClickGameplaySettings);
+        soundSettingsButton.onClick.AddListener(OnClickSoundSettings);
+    }
+
+    private void OnEnable()
+    {
+        SavedSettingData.ApplySetting();
+    }
+
+    private void OnClickDisplaySettings()
+    {
+        displaySettingsGO.SetActive(true);
+        graphicSettingsGO.SetActive(false);
+        gameplaySettingGO.SetActive(false);
+        soundSettingsGO.SetActive(false);
+    }
+
+    private void OnClickGraphicSettings()
+    {
+        displaySettingsGO.SetActive(false);
+        graphicSettingsGO.SetActive(true);
+        gameplaySettingGO.SetActive(false);
+        soundSettingsGO.SetActive(false);
+    }
+
+    private void OnClickGameplaySettings()
+    {
+        displaySettingsGO.SetActive(false);
+        graphicSettingsGO.SetActive(false);
+        gameplaySettingGO.SetActive(true);
+        soundSettingsGO.SetActive(false);
+    }
+
+    private void OnClickSoundSettings()
+    {
+        displaySettingsGO.SetActive(false);
+        graphicSettingsGO.SetActive(false);
+        gameplaySettingGO.SetActive(false);
+        soundSettingsGO.SetActive(true);
+    }
+
+    public void SetApplyOnClickListener(bool isActive, UnityAction listener = null)
+    {
+        applyButton.gameObject.SetActive(isActive);
+        if (isActive)
+        {
+            applyButton.onClick.RemoveAllListeners();
+            applyButton.onClick.AddListener(listener);
+            Debug.Log("Apply 버튼에 리스너 장착 완료");
+        }
+    }
+    public void SetCloseOnClickListener(bool isActive, UnityAction listener = null)
+    {
+        closeButton.gameObject.SetActive(isActive);
+        if (isActive)
+        {
+            closeButton.onClick.RemoveAllListeners();
+            closeButton.onClick.AddListener(listener);
+            Debug.Log("Close 버튼에 리스너 장착 완료");
+        }
+    }
+}
