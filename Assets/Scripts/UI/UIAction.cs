93b80787b85bff405d27c60f8493f3edefd76757
Mingyu Choi <mg010626@naver.com>
2024-12-15 20:58:36 +0900

    Feat | UIAction 스크립트 구현
    esc키 입력을 통한 일시정지 구현

---
+++ Assets/Scripts/UI/UIAction.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class UIAction : MonoBehaviour
+{
+    InputActions action;
+    InputAction pauseAction;
+
+    [SerializeField] private GameObject PausedTabGO;
+
+    private void Awake()
+    {
+        action = GameManager.Instance.Action;
+        action.UI.Enable();
+
+        pauseAction = action.UI.Pause;
+        pauseAction.performed += OnPaused;
+    }
+
+    private void OnDestroy()
+    {
+        action.UI.Disable();
+    }
+
+    private void OnPaused(InputAction.CallbackContext context)
+    {
+        if (PausedTabGO is not null)
+        {
+            PausedTabGO.SetActive(!PausedTabGO.activeSelf);
+            if (PausedTabGO.activeSelf)
+            {
+                GameManager.Instance.PauseGame();
+            }
+            else
+            {
+                GameManager.Instance.ResumeGame();
+            }
+        }
+    }
+
+}
