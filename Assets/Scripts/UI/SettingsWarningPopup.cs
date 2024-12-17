f8abbc8b71a432bdd5f65c4366a8316f1732fca5
Mingyu Choi <mg010626@naver.com>
2024-12-17 17:36:17 +0900

    Feat | 변경된 설정 미반영 후 종료시 나타나는 SettingsWarningPopup 구현

---
+++ Assets/Scripts/UI/SettingsWarningPopup.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class SettingsWarningPopup : EscapeableUI
+{
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private Button okButton;
+    [SerializeField] private Button cancelButton;
+
+    private bool use1Btn = false;
+    private bool isEnableEscape;
+
+    public void DestroyPopup()
+    {
+        Destroy(gameObject);
+    }
+    public void Set2BtnPopup(string title, string description, string okText, string cancelText, 
+        UnityAction okListener, UnityAction cancelListener, bool isEnableEscape = true)
+    {
+        titleText.text = title;
+        descriptionText.text = description;
+        okButton.GetComponentInChildren<TMP_Text>().text = okText;
+        cancelButton.GetComponentInChildren<TMP_Text>().text = cancelText;
+        okButton.onClick.AddListener(okListener);
+        cancelButton.onClick.AddListener(cancelListener);
+        this.isEnableEscape = isEnableEscape;
+    }
+    public void Set2BtnPopup(string title, string description, 
+        UnityAction okListener, UnityAction cancelListener, bool isEnableEscape = true)
+    {
+        titleText.text = title;
+        descriptionText.text = description;
+        okButton.onClick.AddListener(okListener);
+        cancelButton.onClick.AddListener(cancelListener);
+        use1Btn = false;
+        this.isEnableEscape = isEnableEscape;
+    }
+    public void Set1BtnPopup(string title, string description, string okText,
+        UnityAction okListener, bool isEnableEscape = true)
+    {
+        titleText.text = title;
+        descriptionText.text = description;
+        okButton.GetComponentInChildren<TMP_Text>().text = okText;
+        okButton.onClick.AddListener(okListener);
+    }
+    public void Set1BtnPopup(string title, string description,
+        UnityAction okListener, bool isEnableEscape = true)
+    {
+        titleText.text = title;
+        descriptionText.text = description;
+        okButton.onClick.AddListener(okListener);
+    }
+}
