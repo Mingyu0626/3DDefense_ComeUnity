dc39609792649633288ef62a04a43136ba38c3bc
Mingyu Choi <mg010626@naver.com>
2025-01-13 17:05:36 +0900

    Fix | 중복된 게임매니저가 인스턴스의 Action을 변경한 후 삭제되는 이슈 해결
    Singleton 제네릭 클래스의 isDestroyed를 이용하여, 파괴될 게임매니저의 Awake에서는, InputAction을 할당하지 않도록 변경

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -12,7 +12,10 @@ public class GameManager : Singleton<GameManager>
     protected override void Awake()
     {
         base.Awake();
-        Action = new InputActions();
+        if (!isDestroyed)
+        {
+            Action = new InputActions();
+        }
     }
 
     public void EndGame(bool isWin)
