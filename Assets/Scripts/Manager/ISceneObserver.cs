e2c64b778df9111a2639bee0932a8eca30b4b066
Mingyu Choi <mg010626@naver.com>
2025-01-20 17:06:22 +0900

    Add | ISceneObserver 인터페이스 디폴트 메서드 OnSceneClosed 추가

--- Assets/Scripts/Manager/ISceneObserver.cs
+++ Assets/Scripts/Manager/ISceneObserver.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public interface ISceneObserver  
 {
     void OnSceneChanged(string sceneName);
+
+    // 디폴트 메서드
+    void OnSceneClosed(string sceneName)
+    {
+        Debug.Log("UIManager를 제외한 다른 매니저 클래스들");
+    }
 }
