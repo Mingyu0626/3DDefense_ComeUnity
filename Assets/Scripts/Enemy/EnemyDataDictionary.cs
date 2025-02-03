2fcc28108fad036432669493f04126e605b80c9c
Mingyu Choi <mg010626@naver.com>
2025-02-03 16:10:04 +0900

    Feat | EnemyData 데이터 구조체 및 EnemyDataDictionary 적 정보 사전 클래스 구현
    모든 종류의 적 정보를 EnemyDataDictionary 클래스 에서 관리하고, EnemyName enum을 통해 데이터에 접근할 수 있도록 했다.

---
+++ Assets/Scripts/Enemy/EnemyDataDictionary.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyName
+{
+    Slime,
+    TurtleShell
+}
+
+public struct EnemyData
+{
+    private string name;
+
+    private int maxHp;
+    private int curHp;
+    private int attackDamage;
+    private float moveSpeed;
+    private float rotationSpeed;
+
+    private float playerDetactableDistance;
+    private float playerAttackableDistance;
+    private float basementAttackableDistance;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int MaxHP
+    {
+        get { return maxHp; }
+        set { maxHp = value; }
+    }
+    public int CurHP
+    {
+        get { return curHp; }
+        set { curHp = value; }
+    }
+    public int AttackDamage
+    {
+        get { return attackDamage; }
+        set { attackDamage = value; }
+    }
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+    public float RotationSpeed
+    {
+        get { return rotationSpeed; }
+        set {  rotationSpeed = value; }
+    }
+    public float PlayerDetectableDistance
+    {
+        get { return playerDetactableDistance; }
+        set {  playerDetactableDistance = value; }
+    }
+    public float PlayerAttackableDistance
+    {
+        get { return playerAttackableDistance; }
+        set { playerAttackableDistance = value; }
+    }
+    public float BasementAttackableDistance
+    {
+        get { return basementAttackableDistance; }
+        set { basementAttackableDistance = value; }
+    }
+
+    public EnemyData(string name, int maxHp, int curHp, int attackDamage, float moveSpeed, float rotationSpeed,
+        float playerDetactableDistance, float playerAttackableDistance, float basementAttackableDistance)
+    {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.curHp = curHp;
+        this.attackDamage = attackDamage;
+        this.moveSpeed = moveSpeed;
+        this.rotationSpeed = rotationSpeed;
+        this.playerDetactableDistance = playerDetactableDistance;
+        this.playerAttackableDistance = playerAttackableDistance;
+        this.basementAttackableDistance = basementAttackableDistance;
+    }
+}
+
+public static class EnemyDataDictionary
+{
+    private static readonly Dictionary<EnemyName, EnemyData> enemyDataDict = new Dictionary<EnemyName, EnemyData>
+    {
+        { EnemyName.Slime, new EnemyData(nameof(EnemyName.Slime), 1, 1, 1, 8f, 4f, 50f, 20f, 20f) },
+        { EnemyName.TurtleShell, new EnemyData(nameof(EnemyName.TurtleShell), 1, 1, 1, 8f, 4f, 50f, 20f, 20f) }
+    };
+
+    public static EnemyData GetEnemyData(EnemyName type)
+    {
+        return enemyDataDict.TryGetValue(type, out var data) ? data : default;
+    }
+}
+
+
